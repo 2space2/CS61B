f21b71cb0047cbc523b1de87ca3d369b0e3a3e0c
zero <zero112358@yeah.net>
2024-02-05 19:53:55 +0800

    proj1b

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
 
@@ -14,5 +14,24 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+
+    @Test
+    public void testisPalindrome() {
+
+        assertTrue(palindrome.isPalindrome("noon"));
+        assertFalse(palindrome.isPalindrome("hello"));
+        assertTrue(palindrome.isPalindrome("a"));
+        assertTrue(palindrome.isPalindrome(""));
+        assertFalse(palindrome.isPalindrome("afda"));
+    }
+
+    @Test
+    public void testisPalinddrome2() {
+        CharacterComparator offbyone = new OffByOne();
+        assertTrue(palindrome.isPalindrome("ab", offbyone));
+        assertTrue(palindrome.isPalindrome("aabb", offbyone));
+        assertFalse(palindrome.isPalindrome("aaa", offbyone));
+        assertFalse(palindrome.isPalindrome("abcd", offbyone));
+    }
 }
