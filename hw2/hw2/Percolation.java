2fc2bf842bb4fae66aaf46058915558f5bd7aabf
zero <zero112358@yeah.net>
2024-03-09 12:09:44 +0800

    hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,85 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int[][] map;
+    private int numOfOpen;
+    private WeightedQuickUnionUF set;
+    private int length;
+    private int top;
+    private int bottom;
+    private final int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1} };
 
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        map = new int[N][N];
+        length = N;
+        top = N;
+        bottom = N + 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                map[i][j] = 0;
+            }
+        }
+        numOfOpen = 0;
+        set = new WeightedQuickUnionUF(N * N + 2);
+        for (int i = 0; i < N; i++) {
+            set.union(top, XYto1(0, i));
+            set.union(bottom, XYto1(N - 1, i));
+        }
+    }
+    public void open(int row, int col)   {
+        if (!valueCheck(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (map[row][col] == 0) {
+            map[row][col] = 1;
+            numOfOpen++;
+            openCheck(row, col);
+        }
+
+    }
+    public boolean isOpen(int row, int col)  {
+        if (!valueCheck(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return map[row][col] == 1;
+    }
+    public boolean isFull(int row, int col) {
+        if (!valueCheck(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return set.connected(top, XYto1(row, col)) && isOpen(row, col);
+    } // is the site (row, col) full?
+    public int numberOfOpenSites()  {
+        return numOfOpen;
+    }         // number of open sites
+    public boolean percolates() {
+        return set.connected(top, bottom);
+    }             // does the system percolate?
+    private int XYto1(int x, int y) {
+        return x * length + y;
+    }
+    private void openCheck(int row, int col) {
+        for (int[] ints : dir) {
+            int rowCheck = row + ints[0];
+            int colCheck = col + ints[1];
+            if (!valueCheck(rowCheck, colCheck)) {
+                continue;
+            }
+            if (isOpen(rowCheck, colCheck)) {
+                set.union(XYto1(row, col), XYto1(rowCheck, colCheck));
+            }
+        }
+    }
+    private boolean valueCheck(int row, int col) {
+        if (row < 0 || row >= length) {
+            return false;
+        } else return col >= 0 && col < length;
+    }
+    public static void main(String[] args) {
+
+    }  // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,41 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
-
+    private int[] result;
+    private Percolation task;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        result = new int[N];
+        for (int i = 0; i < T ;i++) {
+            task = pf.make(N);
+            int randomX = StdRandom.uniform(N);
+            int randomY = StdRandom.uniform(N);
+            while (!task.percolates()) {
+                while (!task.isOpen(randomX, randomY)) {
+                    randomX = StdRandom.uniform(N);
+                    randomY = StdRandom.uniform(N);
+                }
+                task.open(randomX, randomY);
+            }
+            result[i] =  task.numberOfOpenSites();
+        }
+    }
+    public double mean() {
+        return StdStats.mean(result);
+    }
+    public double stddev() {
+        return StdStats.stddev(result);
+    }
+    public double confidenceLow() {
+        return mean() - 1.96 * Math.sqrt(stddev()) / result.length;
+    }
+    public double confidenceHigh() {
+        return mean() + 1.96 * Math.sqrt(stddev()) / result.length;
+    }
 }
+
+
